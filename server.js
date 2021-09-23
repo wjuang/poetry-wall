4ac7fd5b2f20b68d6d18a3c120056e4f99314378
wjuang <williamsjuang@gmail.com>
2021-09-22 20:07:29 -0400

    server runs with most dependencies running

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+//dotenv setup
+require('dotenv').config()
+const port = process.env.port
+
+//set up public folder (css)
+app.use(express.static('public'))
+
+//set up body parser
+app.use(express.urlencoded({extended: false}))
+
+//set up method override
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+app.listen(port, () => {
+  console.log('app is running on port ' + port)
+})
