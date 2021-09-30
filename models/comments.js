89886c3609497fed5e2813713a7e68beb06a17ec
wjuang <williamsjuang@gmail.com>
2021-09-30 01:29:02 -0400

    changed schemas to bypass write concern

--- models/comments.js
+++ models/comments.js
@@ -4,7 +4,12 @@ const commentSchema = new mongoose.Schema({
   author: {type: String, default: 'Anonymous'},
   comment: {type: String, required: true},
   time: {type: Date, default: Date.now},
-  pageId: {type: String}
+  pageId: {type: String},
+  writeConcern: {
+    w: 'majority',
+    j: true,
+    wtimeout: 1000;
+  }
 })
 
 module.exports = mongoose.model('Comments', commentSchema)
--- models/poems.js
+++ models/poems.js
@@ -4,7 +4,12 @@ const poemSchema = new mongoose.Schema({
   title: {type: String, required: true},
   author: {type: String, default: 'Anonymous'},
   content: {type: String, required: true},
-  time: {type: Date, default: Date.now}
+  time: {type: Date, default: Date.now},
+  writeConcern: {
+    w: 'majority',
+    j: true,
+    wtimeout: 1000;
+  }
 })
 
 module.exports = mongoose.model('Poems', poemSchema)
